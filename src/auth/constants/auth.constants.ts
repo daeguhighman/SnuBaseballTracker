e2dfdf2fdc98c429050947c0bc7cea37a73da605
daeguhighman <keroro19671967@gmail.com>
2025-08-17 20:45:49 +0900

    환경분리

--- src/auth/constants/auth.constants.ts
+++ src/auth/constants/auth.constants.ts
@@ -4,7 +4,10 @@ export const REFRESH_COOKIE = (configService: ConfigService) => ({
   httpOnly: true,
   secure: true, // 반드시 HTTPS
   sameSite: 'lax' as const,
-  domain: 'snubaseball.site',
+  domain:
+    configService.get('NODE_ENV') === 'production'
+      ? 'snubaseball.site'
+      : 'localhost',
   path: '/auth/refresh',
   maxAge: 14 * 24 * 60 * 60 * 1000,
 });
