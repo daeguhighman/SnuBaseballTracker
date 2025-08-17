ce1e12d8b7e3bc7022ba922233561184968e1ae3
daeguhighman <keroro19671967@gmail.com>
2025-08-17 18:49:57 +0900

    쿠키 설정 변경

--- src/auth/constants/auth.constants.ts
+++ src/auth/constants/auth.constants.ts
@@ -4,6 +4,8 @@ export const REFRESH_COOKIE = (configService: ConfigService) => ({
   httpOnly: true,
   secure: true, // 반드시 HTTPS
   sameSite: 'none' as const,
-  path: '/auth/refresh',
+  domain:
+    'process.env.NODE_ENV === "production" ? ".snubaseball.site" : "localhost"',
+  path: '/',
   maxAge: 14 * 24 * 60 * 60 * 1000,
 });
